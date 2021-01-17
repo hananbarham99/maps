b574c623fe67a7b503277d763e65fd45b140e2e7
hananbarham99 <1172605@student.birzeit.edu>
2021-01-17 10:37:14 +0200

    map

---
+++ app/src/main/java/hanan/birzeit/comp438/example/map2/MainActivity.java
@@ -0,0 +1,27 @@
+package hanan.birzeit.comp438.example.map2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button Next = (Button) findViewById(R.id.Next);
+        Button aboutUs = (Button) findViewById(R.id.aboutUs);
+        Next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
